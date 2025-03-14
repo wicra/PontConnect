49bec7c3ba228c158d9f8159419980fc0508094c
Sergio Wicramachine <wicramachine@gmail.com>
2025-03-14 19:11:01 +0100

    feat : Configuration du thème et des constantes

---
+++ lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// COULEURS CENTRALISATION
+const Color primaryColor    = Color(0xFF3CB4AB);
+const Color secondaryColor  = Color(0xFF40499C);
+const Color tertiaryColor   = Color(0xFFFED775);
+const Color accentColor     = Color(0xFFEE5038);
+const Color backgroundLight = Color(0xFFFFFFFF);
+final Color backgroundCream = Color(0xFFe9ecef);
+const Color textPrimary     = Color(0xFF212121);
+const Color textSecondary   = Color(0xFF757575);
+const Color accentColor2    = Color(0xFF6C7AA1);
+const Color backgroundCard  = Color(0xFFedf2fb);
+
+
+// API REST URL CENTRALISATION
+class ApiConstants {
+  static const String baseUrl = 'http://ip:8888/Api/';
+}
